d136dd4c4c0889c5230de47861efaf04a1b736a4
Alex Gorischek <alex.gorischek@gmail.com>
2021-02-07 14:01:54 -0800

    Add TypeScript

---
+++ src/__tests__/badgeRoll.test.ts
@@ -0,0 +1,9 @@
+const assert = require("assert");
+const badgeRoll = require("../index.ts");
+
+describe("badgeRoll", () => {
+  it("has a test", () => {
+    // Placeholder
+    assert(badgeRoll);
+  });
+});
