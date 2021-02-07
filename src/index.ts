bf5d4f48d3d5305e246ba29dd27c2b16ceb4502c
Alex Gorischek <alex.gorischek@gmail.com>
2021-02-07 15:01:54 -0800

    Add initial config loading code

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,27 @@
 "use strict";
 
 module.exports = true;
+
+const { cosmiconfig, cosmiconfigSync } = require('cosmiconfig');
+
+const searchPlaces = [
+  'package.json',
+  `.badgeconfig`,
+  `.badgeconfig.json`,
+  `.badgeconfig.yaml`,
+  `.badgeconfig.yml`,
+  `.badgeconfig.js`,
+  `.badgeconfig.cjs`,
+  `badge.config.js`,
+  `$badge.config.cjs`,
+]
+
+const explorer = cosmiconfig("badgeConfig", {searchPlaces: searchPlaces});
+
+explorer.search()
+  .then((result: any) => {
+    console.log(result)
+  })
+  .catch((error: any) => {
+    console.log(error)
+  });
