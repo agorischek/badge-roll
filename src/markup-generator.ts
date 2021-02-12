590d78e72c7fa8a2eea481bc9634db6bf64da0f1
Alex Gorischek <alex.gorischek@gmail.com>
2021-02-11 17:01:37 -0800

    Rename generator and remove old tests

---
+++ src/markup-generator.ts
@@ -0,0 +1,81 @@
+"use strict";
+
+import unified from "unified";
+import stringify from "remark-stringify";
+import { Node } from "unist";
+
+const generator = unified().use(stringify);
+
+function generateMarkdown(node: Node) {
+  const generated = generator.stringify(node);
+  return generated;
+}
+
+function generateImageMarkup(
+  language: string,
+  url: string,
+  alt: string,
+  title: string
+) {
+  if (language === "markdown") {
+    const imageNode: Node = {
+      type: "image",
+      url: url,
+      title: title,
+      alt: alt,
+    };
+    imageNode;
+    const generatedImageMarkup = generateMarkdown(imageNode);
+    return generatedImageMarkup;
+  } else {
+    throw "Unsupported markup language";
+  }
+}
+
+function generateLinkMarkup(
+  language: string,
+  url: string,
+  title: string,
+  display: string
+) {
+  if (language === "markdown") {
+    const linkNode: Node = {
+      type: "link",
+      url: url,
+      title: title,
+      children: { type: "text", value: display },
+    };
+    const generatedLinkMarkup = generateMarkdown(linkNode);
+    return generatedLinkMarkup;
+  } else {
+    throw "Unsupported markup language";
+  }
+}
+
+export function generateBadgeMarkup(
+  language: string,
+  imageUrl: string,
+  targetUrl: string,
+  alt: string
+): string {
+  if (language === "markdown") {
+    alt;
+    const generatedImageMarkup = generateImageMarkup(
+      language,
+      imageUrl,
+      alt,
+      null
+    );
+    generatedImageMarkup;
+    const generatedBadgeMarkup = generateLinkMarkup(
+      language,
+      targetUrl,
+      alt,
+      generatedImageMarkup
+    );
+    generatedBadgeMarkup;
+    return generatedBadgeMarkup;
+  } else {
+    throw "Unsupported markup language";
+  }
+}
