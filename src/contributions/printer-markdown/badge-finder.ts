b37effa81a6e67999c17557322c55d93953181ff
Alex Gorischek <alex.gorischek@gmail.com>
2021-02-21 16:11:43 -0800

    Remove unneeded imports

--- src/contributions/printer-markdown/badge-finder.ts
+++ src/contributions/printer-markdown/badge-finder.ts
@@ -1,6 +1,5 @@
 import unified from "unified";
 import markdown from "remark-parse";
-import remark2rehype from "remark-rehype";
 const parents = require("unist-util-parents");
 const find = require("unist-util-find");
 const is = require("unist-util-is");
@@ -11,13 +10,13 @@ import { wrappedNodeMatchesPattern } from "./badge-tester";
 import { badgeSectionLocation, badgeFinderState } from "./types";
 
 export function findBadgeSection(doc: string): badgeSectionLocation {
-  const processor = unified().use(markdown).use(remark2rehype);
+  const processor = unified().use(markdown);
   const tree = processor.parse(doc);
   const treeWithParents = parents(tree);
   const firstBadge = find(treeWithParents, wrappedNodeMatchesPattern);
   const firstBadgeParent = firstBadge.parent;
 
-  let state = new badgeFinderState(firstBadge, firstBadgeParent);
+  const state = new badgeFinderState(firstBadge, firstBadgeParent);
 
   while (!state.lastBadge) {
     const currentNodeIsBadge = is(state.currentNode, wrappedNodeMatchesPattern);
