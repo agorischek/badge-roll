b61003d7852352ca6e74a8acfca39ffaf266ccf9
Alex Gorischek <alex.gorischek@gmail.com>
2021-02-22 17:35:02 -0800

    Wrap core of library in `affix` function

--- src/index.ts
+++ src/index.ts
@@ -9,24 +9,25 @@ import {
   Target,
 } from "./types";
 
-export default true;
+export function affix(source: string, configData: Config): string {
+  const config = new Config(configData);
+  const contributions = new ContributionSet();
+  const settings = new Settings(config, contributions);
+  const about = new About(config, contributions);
+  const providers = new ProvidersDirectory(contributions);
+  const printers = new PrinterList(contributions);
+  const target = new Target(settings, source);
 
-const config = new Config();
-const contributions = new ContributionSet();
-const settings = new Settings(config, contributions);
-const about = new About(config, contributions);
-const providers = new ProvidersDirectory(contributions);
+  const printer = printers[target.printer];
+  const badges = config.badges.map(
+    (badge) => new Badge(badge, settings, about, providers)
+  );
+  const markup = printer(badges, settings, target.originalContent);
+  return markup;
+}
 
-const badges = config.badges.map(
-  (badge) => new Badge(badge, settings, about, providers)
-);
-
-const target = new Target(settings);
-const extension = target.extension;
-
-const printers = new PrinterList(contributions);
-const printer = printers[extension];
-
-const markup = printer(badges, settings, target.originalContent);
-
-target.write(markup);
+export function check(source: string, config: Config): boolean {
+  const affixed = affix(source, config);
+  const match = affixed === source;
+  return match;
+}
