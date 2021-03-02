fca683dc94e2091d73b6441cc5b41eb9c4040b57
Alex Gorischek <alex.gorischek@gmail.com>
2021-03-01 19:13:57 -0800

    Add some Appveyor badges

--- src/contributions/provider-shields/index.ts
+++ src/contributions/provider-shields/index.ts
@@ -18,7 +18,36 @@ export default {
           to: ":packageHomepage",
           display: "Downloads",
         },
+        "appveyor/build": {
+          details: ":user/:repo",
+          to: "",
+          display: "Build",
+          variations: {
+            branch: {
+              details: ":user/:repo/:branch",
+            },
+          },
+        },
+        "appveyor/job/build": {
+          details: ":user/:repo/:job",
+          to: "",
+          display: "Job",
+          variations: {
+            branch: {
+              details: ":user/:repo/:job/:branch",
+            },
+          },
+        },
       },
     },
   },
 };
+
+// AppVeyor:	/appveyor/build/:user/:repo
+// AppVeyor branch:		/appveyor/build/:user/:repo/:branch
+// AppVeyor Job:		/appveyor/job/build/:user/:repo/:job
+// AppVeyor Job branch:		/appveyor/job/build/:user/:repo/:job/:branch
+// AppVeyor tests:		/appveyor/tests/:user/:repo
+// AppVeyor tests (branch):		/appveyor/tests/:user/:repo/:branch
+// AppVeyor tests (compact):		/appveyor/tests/:user/:repo?compact_message
+// AppVeyor tests with custom labels:	/appveyor/tests/:user/:repo?failed_label=bad&passed_label=good&skipped_label=n%2Fa
