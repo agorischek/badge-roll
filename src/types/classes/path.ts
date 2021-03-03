6c305a4dfddb41748d72cead7a067e9beadac62e
Alex Gorischek <alex.gorischek@gmail.com>
2021-03-02 19:46:36 -0800

    Compress if/else syntax

--- src/types/classes/path.ts
+++ src/types/classes/path.ts
@@ -14,17 +14,11 @@ export class Path {
 export function evaluatePath(path: Array<PathSegment>, about: About): string {
   const evaluated = path.reduce((acc: string, segment: PathSegment) => {
     if (segment.kind === "literal") {
-      if (acc === "") {
-        return `${segment.name}`;
-      } else {
-        return `${acc}/${segment.name}`;
-      }
+      if (acc === "") return `${segment.name}`;
+      else return `${acc}/${segment.name}`;
     } else if (segment.kind === "variable") {
-      if (acc === "") {
-        return `${about[segment.name]}`;
-      } else {
-        return `${acc}/${about[segment.name]}`;
-      }
+      if (acc === "") return `${about[segment.name]}`;
+      else return `${acc}/${about[segment.name]}`;
     }
   }, "");
   return evaluated;
@@ -34,11 +28,8 @@ export function parsePath(unparsed: string): Array<PathSegment> {
   const split = unparsed.split("/");
   const path: Array<PathSegment> = split.map((segment) => {
     const matched = segment.match(/^:(.+)/);
-    if (matched) {
-      return { kind: "variable", name: matched[1] };
-    } else {
-      return { kind: "literal", name: segment };
-    }
+    if (matched) return { kind: "variable", name: matched[1] };
+    else return { kind: "literal", name: segment };
   });
   return path;
 }
