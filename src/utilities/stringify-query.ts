bc178ca8d7f672c2de850e826a62dd51149b0016
Alex Gorischek <alex.gorischek@gmail.com>
2021-03-08 18:59:39 -0800

    Handle false query params

--- src/utilities/stringify-query.ts
+++ src/utilities/stringify-query.ts
@@ -3,7 +3,8 @@ import { QueryParams } from "../types";
 export function stringifyQuery(queryParams: QueryParams): string {
   return Object.keys(queryParams).reduce((acc, key) => {
     const value = queryParams[key];
-    const stringified = value === true ? key : `${key}=${value}`;
+    const stringified =
+      value === true ? key : value === false ? "" : `${key}=${value}`;
     return acc ? `${acc}&${stringified}` : `${stringified}`;
   }, "");
 }
