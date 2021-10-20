aa0e4914130799e08bb9bb9f961766786172bd66
Alex Gorischek <alex.gorischek@gmail.com>
2021-10-19 20:56:43 -0700

    Fix unist-util-find typings

--- src/__tests__/about.test.ts
+++ src/__tests__/about.test.ts
@@ -1,15 +1,11 @@
 import { About, ContributionSet } from "../types/index.js";
 
 describe("About", () => {
-  // test.only("duh", () => {
-  //   expect(true).toBe(false);
-  // });
-  test.only("should correctly retrieve the package name", async () => {
+  test("should correctly retrieve the package name", async () => {
     const contributions = new ContributionSet();
     await contributions.load();
     const config = {};
     const about = new About(config, contributions);
-    console.log("package name is" + about.packageName);
     expect(about.packageName).toBe("badge-roll");
   });
   test("should correctly retrieve the user name", async () => {
--- src/types/imports/unist.d.ts
+++ src/types/imports/unist.d.ts
@@ -1,8 +1,12 @@
 declare module "unist-util-find" {
   import { Node } from "unist";
+  type ChildNode = Node & { parent: Parent };
   import { Parent } from "mdast";
   type example = { [key: string]: string | number };
   type condition = (node: Node) => boolean;
-  function find(node: Node, test: string | example | condition | Parent): Node;
+  function find(
+    node: Node,
+    test: string | example | condition | Parent
+  ): ChildNode;
   export = find;
 }
