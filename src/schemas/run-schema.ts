dab57a05951148141cad052bb41addb2e15793fa
Alex Gorischek <alex.gorischek@gmail.com>
2021-11-07 04:11:34 +0000

    Fix leftover dir import

--- src/schemas/run-schema.ts
+++ src/schemas/run-schema.ts
@@ -1,7 +1,7 @@
 import Joi from "joi";
 const { boolean, object, string } = Joi.types();
 
-import { configSchema } from ".";
+import { configSchema } from "./config-schema.js";
 
 export const runSchema = object.keys({
   original: string,
