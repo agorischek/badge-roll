420bef90cda66c86576e62d05c18cf2a76fba023
Alex Gorischek <alex.gorischek@gmail.com>
2021-11-07 15:34:09 +0000

    Restyle unist-util-find declaration

--- src/types/imports/unist.d.ts
+++ src/types/imports/unist.d.ts
@@ -1,12 +1,15 @@
 declare module "unist-util-find" {
   import { Node } from "unist";
-  type ChildNode = Node & { parent: Parent };
   import { Parent } from "mdast";
+
+  type ChildNode = Node & { parent: Parent };
   type example = { [key: string]: string | number };
   type condition = (node: Node) => boolean;
+
   function find(
     node: Node,
     test: string | example | condition | Parent
   ): ChildNode;
+
   export = find;
 }
